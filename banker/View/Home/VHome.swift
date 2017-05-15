930e69c8d34a2cfc8f5b05562ad5383ef71c090a
vauxhall <zero.griffin@gmail.com>
2017-05-15 15:24:53 -0500

    gradient background

--- banker/View/Home/VHome.swift
+++ banker/View/Home/VHome.swift
@@ -14,14 +14,27 @@ class VHome:VView
         super.init(controller:controller)
         self.controller = controller as? CHome
         
+        let background:VHomeBackground = VHomeBackground()
+        
         let viewBar:VHomeBar = VHomeBar(controller:self.controller)
         self.viewBar = viewBar
         
         let viewMenu:VHomeMenu = VHomeMenu(controller:self.controller)
         self.viewMenu = viewMenu
         
+        addSubview(background)
         addSubview(viewBar)
         addSubview(viewMenu)
+        
+        NSLayoutConstraint.topToTop(
+            view:background,
+            toView:viewBar)
+        NSLayoutConstraint.bottomToBottom(
+            view:background,
+            toView:viewMenu)
+        NSLayoutConstraint.equalsHorizontal(
+            view:background,
+            toView:self)
         
         layoutBarTop = NSLayoutConstraint.topToTop(
             view:viewBar,
--- banker/View/Home/VHomeBackground.swift
+++ banker/View/Home/VHomeBackground.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class VHomeBackground:UIView
 {
+    private let kLocationTop:NSNumber = 0
+    private let kLocationBottom:NSNumber = 1
+    
     init()
     {
         super.init(frame:CGRect.zero)
@@ -9,10 +12,42 @@ class VHomeBackground:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         isUserInteractionEnabled = false
+        
+        guard
+            
+            let gradientLayer:CAGradientLayer = self.layer as? CAGradientLayer
+            
+        else
+        {
+            return
+        }
+        
+        let topColor:CGColor = UIColor.bankerGray.cgColor
+        let bottomColor:CGColor = UIColor(
+            red:0.047058823529412,
+            green:0.141176470588235,
+            blue:0.196078431372549,
+            alpha:1).cgColor
+        
+        gradientLayer.locations = [
+            kLocationTop,
+            kLocationBottom]
+        
+        gradientLayer.colors = [
+            topColor,
+            bottomColor]
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override open class var layerClass:AnyClass
+    {
+        get
+        {
+            return CAGradientLayer.self
+        }
     }
 }
