9b1df2b6ea3d5449d03ecd0e44c997edf048e2d4
vauxhall <zero.griffin@gmail.com>
2017-05-16 11:36:26 -0500

    keyboard number 2

--- banker/Model/Keyboard/Items/MKeyboardNumber2.swift
+++ banker/Model/Keyboard/Items/MKeyboardNumber2.swift
@@ -1,9 +1,11 @@
-//
-//  MKeyboardNumber2.swift
-//  banker
-//
-//  Created by zero on 5/16/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MKeyboardNumber2:MKeyboardNumber
+{
+    private let kNumber:String = "2"
+    
+    init()
+    {
+        super.init(number:kNumber, image:#imageLiteral(resourceName: "assetKeyboard2"))
+    }
+}
