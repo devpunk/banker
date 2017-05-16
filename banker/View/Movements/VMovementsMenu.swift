d87001f4ac45ee63693994e2cbd2beadcb713667
vauxhall <zero.griffin@gmail.com>
2017-05-16 15:27:10 -0500

    back button

--- banker/View/Movements/VMovementsMenu.swift
+++ banker/View/Movements/VMovementsMenu.swift
@@ -3,6 +3,7 @@ import UIKit
 class VMovementsMenu:UIView
 {
     private weak var controller:CMovements!
+    private let kBackWidth:CGFloat = 50
     
     init(controller:CMovements)
     {
@@ -11,6 +12,30 @@ class VMovementsMenu:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let backButton:UIButton = UIButton()
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.setImage(
+            #imageLiteral(resourceName: "assetGenericBack").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        backButton.setImage(
+            #imageLiteral(resourceName: "assetGenericBack").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        backButton.imageView!.tintColor = UIColor(white:0, alpha:0.2)
+        backButton.imageView!.clipsToBounds = true
+        backButton.imageView!.contentMode = UIViewContentMode.center
+        
+        addSubview(backButton)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:backButton,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:backButton,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:backButton,
+            constant:kBackWidth)
     }
     
     required init?(coder:NSCoder)
---
+++ banker/View/Movements/VMovementsMenuTypeItem.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VMovementsMenuTypeItem:UIButton
+{
+    
+}
