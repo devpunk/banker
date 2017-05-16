73c00846b32d511956880e89d88ef5de8239baed
vauxhall <zero.griffin@gmail.com>
2017-05-16 08:41:44 -0500

    added view field

--- banker/View/Movements/VMovements.swift
+++ banker/View/Movements/VMovements.swift
@@ -3,11 +3,19 @@ import UIKit
 class VMovements:VView
 {
     private weak var controller:CMovements!
+    private weak var viewField:VMovementsField!
+    private let kFieldHeight:CGFloat = 100
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CMovements
+        
+        let viewField:VMovementsField = VMovementsField()
+        
+        self.viewField = viewField
+        
+        addSubview(viewField)
     }
     
     required init?(coder:NSCoder)
