c2a867086b5b06724f0c56dba7f043febb35e858
vauxhall <zero.griffin@gmail.com>
2017-05-16 16:50:38 -0500

    menu type items

--- banker/View/Movements/VMovementsMenuType.swift
+++ banker/View/Movements/VMovementsMenuType.swift
@@ -3,6 +3,9 @@ import UIKit
 class VMovementsMenuType:UIView
 {
     private weak var controller:CMovements!
+    private weak var itemDeposit:VMovementsMenuTypeItem!
+    private weak var itemExpense:VMovementsMenuTypeItem!
+    private weak var layoutExpenseWidth:NSLayoutConstraint!
     
     init(controller:CMovements)
     {
@@ -11,10 +14,49 @@ class VMovementsMenuType:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let itemDeposit:VMovementsMenuTypeItem = VMovementsMenuTypeItem(
+            title:NSLocalizedString("VMovementsMenuType_itemDeposit", comment:""))
+        self.itemDeposit = itemDeposit
+        
+        let itemExpense:VMovementsMenuTypeItem = VMovementsMenuTypeItem(
+            title:NSLocalizedString("VMovementsMenuType_itemExpense", comment:""))
+        self.itemExpense = itemExpense
+        
+        addSubview(itemExpense)
+        addSubview(itemDeposit)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:itemExpense,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:itemExpense,
+            toView:self)
+        layoutExpenseWidth = NSLayoutConstraint.width(
+            view:itemExpense)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:itemDeposit,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:itemDeposit,
+            toView:itemExpense)
+        NSLayoutConstraint.rightToRight(
+            view:itemDeposit,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.size.width
+        let width_2:CGFloat = width / 2.0
+        layoutExpenseWidth.constant = width_2
+        
+        super.layoutSubviews()
     }
 }
--- banker/View/Movements/VMovementsMenuTypeItem.swift
+++ banker/View/Movements/VMovementsMenuTypeItem.swift
@@ -2,5 +2,24 @@ import UIKit
 
 class VMovementsMenuTypeItem:UIButton
 {
+    init(title:String)
+    {
+        super.init(frame:CGRect.zero)
+        setTitle(title, for:UIControlState.normal)
+        setTitleColor(
+            UIColor.white,
+            for:UIControlState.highlighted)
+        setTitleColor(
+            UIColor.white,
+            for:UIControlState.selected)
+        setTitleColor(
+            UIColor.bankerOrange,
+            for:UIControlState.normal)
+        titleLabel!.font = UIFont.regular(size:13)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
