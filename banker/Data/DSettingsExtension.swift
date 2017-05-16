dd38c747255c81e219431a479af07ec6247f1408
vauxhall <zero.griffin@gmail.com>
2017-05-16 12:30:17 -0500

    currency symbol is optional

--- banker/Data/DSettingsExtension.swift
+++ banker/Data/DSettingsExtension.swift
@@ -2,11 +2,27 @@ import Foundation
 
 extension DSettings
 {
+    private static let kDefaultCurrency:String = "$"
+    
     //MARK: public
     
     func addTtl()
     {
         ttl += 1
         DManager.sharedInstance?.save()
+    }
+    
+    func currency() -> String
+    {
+        guard
+        
+            let currencySymbol:String = self.currencySymbol
+        
+        else
+        {
+            return DSettings.kDefaultCurrency
+        }
+        
+        return currencySymbol
     }
 }
